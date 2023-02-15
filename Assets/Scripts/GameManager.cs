a7aaa11f9ba9d9654a565c20c10769386ce4d07d
Muhammad Ammar Naeem <ammar.naeem@tintash.com>
2023-02-15 15:44:15 +0500

    test1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("Check1");
+    }
+
     public bool IsTrue()
     {
         return true;
